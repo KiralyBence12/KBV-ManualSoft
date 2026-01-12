415dffa7bc3a1a1586cc7f5d573602aace9f16e6
KiralyBence12 <118981185+KiralyBence12@users.noreply.github.com>
2026-01-12 09:31:11 +0100

    update

--- KBV-ManualSoft/KBV-ManualSoft/Form1.Designer.cs
+++ KBV-ManualSoft/KBV-ManualSoft/Form1.Designer.cs
@@ -28,13 +28,94 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "GoPro Hero",
+            "HIKMICRO LYNX Hőkamera",
+            "Beko hűtő",
+            "IPhone",
+            "BenQ Monitor",
+            "Tesla Mosógép",
+            "Gorenje Szárítógép",
+            "Samsung TV",
+            "AirFryer",
+            "DeLonghi kávéfőző"});
+            this.listBox1.Location = new System.Drawing.Point(12, 46);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(306, 277);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 329);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Hozzáadás";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(243, 329);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Törlés";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(589, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "GARANCIA";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(566, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(208, 334);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- KBV-ManualSoft/KBV-ManualSoft/Form1.cs
+++ KBV-ManualSoft/KBV-ManualSoft/Form1.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +17,97 @@ namespace KBV_ManualSoft
         public Form1()
         {
             InitializeComponent();
+            label1.Visible = false;
+            label2.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "PDF files (*.pdf)|*.pdf|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 1;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string filePath = openFileDialog.FileName;
+                    string fileName = Path.GetFileName(filePath);
+
+                    listBox1.Items.Add(fileName);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+            {
+                listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+            }
+            else
+            {
+                MessageBox.Show("Kérlek válassz ki egy elemet amit törölni szeretnél.");
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            if (listBox1.SelectedItem != null)
+            {
+                string filePath = (listBox1.SelectedItem.ToString() + ".pdf");
+                using (var chooseForm = new ChooseDocumentForm())
+                {
+                    DialogResult result = chooseForm.ShowDialog();
+                    label1.Visible = true;
+                    label2.Visible = true;
+                    if (result == DialogResult.Yes)
+                    {
+                        switch (listBox1.SelectedIndex)
+                        {
+                            case 0:
+                                label2.Text = "GoPro Hero – Garancia\nVásárlás dátuma: 2023.06.15\nVásárlás helye: MediaMarkt\nVételár: 149 990 Ft\nGarancia: 3 év\nSzerviz: GoPro hivatalos";
+                            break;
+
+                            case 1:
+                                label2.Text = "HIKMICRO LYNX – Garancia\nVásárlás dátuma: 2022.11.02\nVásárlás helye: hivatalos forgalmazó\nVételár: 329 000 Ft\nGarancia: 2 év\nSzerviz: HIKMICRO";
+                                break;
+                            case 2:
+                                label2.Text = "Beko hűtőszekrény – Garancia\nVásárlás dátuma: 2021.08.20\nVásárlás helye: Euronics\nVételár: 189 990 Ft\nGarancia: 2 év\nSzerviz: Beko márkaszerviz";
+                                break;
+                            case 3:
+                                label2.Text = "iPhone – Garancia\nVásárlás dátuma: 2024.01.10\nVásárlás helye: Apple Store\nVételár: 399 990 Ft\nGarancia: 3 év\nSzerviz: Apple hivatalos";
+                                break;
+                            case 4:
+                                label2.Text = "BenQ Monitor – Garancia\nVásárlás dátuma: 2020.05.05\nVásárlás helye: Alza\nVételár: 119 990 Ft\nGarancia: 3 év\nSzerviz: BenQ márkaszerviz";
+                                break;
+                            case 5:
+                                label2.Text = "Tesla mosógép – Garancia\nVásárlás dátuma: 2022.03.18\nVásárlás helye: MediaMarkt\nVételár: 169 990 Ft\nGarancia: 2 év\nSzerviz: Tesla hivatalos";
+                                break;
+                            case 6:
+                                label2.Text = "Gorenje szárítógép – Garancia\nVásárlás dátuma: 2021.12.01\nVásárlás helye: BestByte\nVételár: 219 990 Ft\nGarancia: 3 év\nSzerviz: Gorenje márkaszerviz";
+                                break;
+                            case 7:
+                                label2.Text = "Samsung TV – Garancia\nVásárlás dátuma: 2023.02.14\nVásárlás helye: Euronics\nVételár: 279 990 Ft\nGarancia: 2 év\nSzerviz: Samsung hivatalos";
+                                break;
+                            case 8:
+                                label2.Text = "AirFryer – Garancia\nVásárlás dátuma: 2024.04.22\nVásárlás helye: Lidl\nVételár: 49 990 Ft\nGarancia: 1 év\nSzerviz: Lidl szervizpartner";
+                                break;
+                            case 9:
+                                label2.Text = "DeLonghi kávéfőző – Garancia\nVásárlás dátuma: 2022.09.09\nVásárlás helye: MediaMarkt\nVételár: 159 990 Ft\nGarancia: 2 év\nSzerviz: DeLonghi márkaszerviz";
+                                break;
+                        }
+                    }
+                    else if (result == DialogResult.No)
+                    {
+                        Process.Start(filePath);
+                    }
+                }
+
+            }
+
         }
     }
 }
